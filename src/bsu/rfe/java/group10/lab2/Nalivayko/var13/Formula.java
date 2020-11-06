9e87b13dd4a9269e61a01ff7962eb96cdd19a1e3
Polina <nalivayko.popalina@gmail.com>
2020-11-06 13:52:21 +0300

    made M+ button work, added two more memory variables, changed calculate1 method

--- src/bsu/rfe/java/group10/lab2/Nalivayko/var13/Formula.java
+++ src/bsu/rfe/java/group10/lab2/Nalivayko/var13/Formula.java
@@ -2,23 +2,31 @@ package bsu.rfe.java.group10.lab2.Nalivayko.var13;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionListener;
+
+import static java.lang.Math.exp;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.cos;
+import static java.lang.Math.log;
 
 public class Formula extends JFrame {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 320;
     private int formulaId = 1;
-    private int memId=1;
-    private double memSum=0;
+    private int memId = 1;
+    private double memSum1 = 0;
+    private double memSum2 = 0;
+    private double memSum3 = 0;
+
 
     Formula() {
-setSize(400, 400);
-        JTextField textFieldX = createTextField("0");
-        JLabel labelforX= new JLabel("X:");
-        JTextField textFieldY = createTextField("0");
-        JLabel labelforY= new JLabel("Y:");
-        JTextField textFieldZ = createTextField("0");
-        JLabel labelforZ= new JLabel("Z:");
+        setSize(WIDTH, HEIGHT);
+        JTextField textFieldX = createTextField();
+        JLabel labelforX = new JLabel("X:");
+        JTextField textFieldY = createTextField();
+        JLabel labelforY = new JLabel("Y:");
+        JTextField textFieldZ = createTextField();
+        JLabel labelforZ = new JLabel("Z:");
         Box textFieldBox = Box.createHorizontalBox();
         textFieldBox.add(Box.createHorizontalGlue());
         textFieldBox.add(labelforX);
@@ -33,72 +41,61 @@ setSize(400, 400);
         textFieldBox.add(Box.createHorizontalGlue());
         textFieldBox.add(textFieldZ);
         textFieldBox.add(Box.createHorizontalGlue());
-        JRadioButton func1=new JRadioButton("Formula 1");
+        JRadioButton func1 = new JRadioButton("Formula 1");
         func1.setSelected(true);
-        JRadioButton func2=new JRadioButton("Formula 2");
+        JRadioButton func2 = new JRadioButton("Formula 2");
         ButtonGroup radioButtons = new ButtonGroup();
         radioButtons.add(func1);
-        func1.addActionListener(actionEvent->{
-            formulaId=1;
-        });
+        func1.addActionListener(actionEvent -> formulaId = 1);
         radioButtons.add(func2);
-        func2.addActionListener(actionEvent->{
-            formulaId=2;
-        });
+        func2.addActionListener(actionEvent -> formulaId = 2);
 
-        JTextField result=createTextField("0");
-        Box boxResult=Box.createHorizontalBox();
+        JTextField result = createTextField();
+        Box boxResult = Box.createHorizontalBox();
         boxResult.add(Box.createHorizontalGlue());
         boxResult.add(result);
         boxResult.add(Box.createHorizontalGlue());
 
         JButton buttonClean = new JButton("Очистить поле");
-        buttonClean.addActionListener(actionEvent->{
-            textFieldX.setText(String.valueOf("0"));
-            textFieldY.setText(String.valueOf("0"));
-            textFieldZ.setText(String.valueOf("0"));
-            result.setText(String.valueOf("0"));
+        buttonClean.addActionListener(actionEvent -> {
+            textFieldX.setText("0");
+            textFieldY.setText("0");
+            textFieldZ.setText("0");
+            result.setText("0");
         });
 
         JButton buttonCalc = new JButton("Вычислить");
-        buttonCalc.addActionListener(actionEvent->{
-          try { double x = Double.parseDouble(textFieldX.getText());
+        buttonCalc.addActionListener(actionEvent -> {
+            try {
+                double x = Double.parseDouble(textFieldX.getText());
                 double y = Double.parseDouble(textFieldY.getText());
                 double z = Double.parseDouble(textFieldZ.getText());
-                if(formulaId==1){
-                    result.setText(String.valueOf(calculate1(x,y,z)));
+                if (formulaId == 1) {
+                    result.setText(String.valueOf(calculate1(x, y, z)));
+                } else {
+                    result.setText(String.valueOf(calculate2(x, y, z)));
                 }
-                else{
-                    result.setText(String.valueOf(calculate2(x,y,z)));}
-          } catch(NumberFormatException ololo){
+            } catch (NumberFormatException ololo) {
                 JOptionPane.showMessageDialog(Formula.this, "Ошибка в " +
-                            "формате записи числа с плавающей точкой", "Ошибочный формат числа",
-                    JOptionPane.WARNING_MESSAGE);
+                                "формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                        JOptionPane.WARNING_MESSAGE);
 
-                }
+            }
         });
 
-        JRadioButton mem1=new JRadioButton("mem1");
+        JRadioButton mem1 = new JRadioButton("mem1");
         mem1.setSelected(true);
-        mem1.addActionListener((actionEvent->{
-            memSum = Double.parseDouble(result.getText());
-            memId=1;
-        }));
-        JRadioButton mem2=new JRadioButton("mem2");
-        mem2.addActionListener(actionEvent->{
-            memSum = Double.parseDouble(result.getText());
-            memId=2;
-        });
-        JRadioButton mem3=new JRadioButton("mem3");
-        mem3.addActionListener(actionEvent->{
-            memSum = Double.parseDouble(result.getText());
-            memId=3;
-        });
-        ButtonGroup memGroup=new ButtonGroup();
+        memId = 1;
+        mem1.addActionListener((actionEvent -> memId = 1));
+        JRadioButton mem2 = new JRadioButton("mem2");
+        mem2.addActionListener(actionEvent -> memId = 2);
+        JRadioButton mem3 = new JRadioButton("mem3");
+        mem3.addActionListener(actionEvent -> memId = 3);
+        ButtonGroup memGroup = new ButtonGroup();
         memGroup.add(mem1);
         memGroup.add(mem2);
         memGroup.add(mem3);
-        Box memBox=Box.createHorizontalBox();
+        Box memBox = Box.createHorizontalBox();
         memBox.add(Box.createHorizontalGlue());
         memBox.add(mem1);
         memBox.add(Box.createHorizontalGlue());
@@ -107,30 +104,39 @@ setSize(400, 400);
         memBox.add(mem3);
         memBox.add(Box.createHorizontalGlue());
 
-        JButton buttonM=new JButton("M+");
-        buttonM.addActionListener(actionEvent->{
-            double counterM= Double.parseDouble(result.getText());
-            if(memId==1){
-                counterM+=memSum;
-                result.setText(String.valueOf(Double.valueOf(counterM)));
+        JButton buttonM = new JButton("M+");
+        buttonM.addActionListener(actionEvent -> {
+            double counterM = Double.parseDouble(result.getText());
+            if (memId == 1) {
+                memSum1 += counterM;
+                System.out.println(memSum1);
+                result.setText(String.valueOf(Double.valueOf(memSum1)));
             }
-            if(memId==2){
-                counterM+=memSum;
-                result.setText(String.valueOf(counterM));
+            if (memId == 2) {
+                memSum2 += counterM;
+                result.setText(String.valueOf(memSum2));
             }
-            if(memId==3){
-                counterM+=memSum;
-                result.setText(String.valueOf(counterM));
+            if (memId == 3) {
+                memSum3 += counterM;
+                result.setText(String.valueOf(memSum3));
             }
-
         });
 
-        JButton buttonMC=new JButton("MC");
-        buttonM.addActionListener(actionEvent->{
-            memSum=0;
+        JButton buttonMC = new JButton("MC");
+        buttonMC.addActionListener(actionEvent -> {
+            if (memId == 1) {
+                memSum1 = 0;
+            }
+            if (memId == 2) {
+                memSum2 = 0;
+            }
+            if (memId == 3) {
+                memSum3 = 0;
+            }
+            result.setText("0");
         });
 
-        Box bBBbox=Box.createHorizontalBox();
+        Box bBBbox = Box.createHorizontalBox();
         bBBbox.add(Box.createHorizontalGlue());
         bBBbox.add(buttonCalc);
         bBBbox.add(Box.createHorizontalGlue());
@@ -142,8 +148,6 @@ setSize(400, 400);
         bBBbox.add(Box.createHorizontalGlue());
 
 
-
-
         Box hboxFormulaType = Box.createHorizontalBox();
         hboxFormulaType.add(Box.createHorizontalGlue());
         hboxFormulaType.add(func1);
@@ -152,7 +156,7 @@ setSize(400, 400);
         hboxFormulaType.add(Box.createHorizontalGlue());
 
 
-        Box ololo=Box.createVerticalBox();
+        Box ololo = Box.createVerticalBox();
         ololo.add(Box.createVerticalGlue());
         ololo.add(textFieldBox);
         ololo.add(Box.createHorizontalStrut(50));
@@ -167,19 +171,20 @@ setSize(400, 400);
         this.getContentPane().add(ololo);
     }
 
-    // Формула №1 для рассчѐта
-    public Double calculate1(Double x, Double y,Double z) {
-        return x + y + z;
+    public Double calculate1(Double x, Double y, Double z) {
+        double first = cos(exp(x)) + log(1 + y) * log(1 + y);
+        double second = pow(exp(cos(x)) + sin(Math.PI * z) * sin(Math.PI * z), 0.5);
+        double third = pow(1 / x, 0.5) + cos(y * y);
+        return pow(first + second + third, sin(z));
     }
 
-    // Формула №2 для рассчѐта
     public Double calculate2(Double x, Double y, Double z) {
-        return x - y -z;
+        return (pow((1 + x * x), 1 / y)) / exp(sin(z) + x);
     }
 
-    private JTextField createTextField(String name){
-        JTextField field = new JTextField(name);
-        Dimension dimension = new Dimension(10*field.getPreferredSize().width, field.getPreferredSize().height);
+    private JTextField createTextField() {
+        JTextField field = new JTextField("0");
+        Dimension dimension = new Dimension(10 * field.getPreferredSize().width, field.getPreferredSize().height);
         field.setMaximumSize(dimension);
         field.setMinimumSize(dimension);
         field.setPreferredSize(dimension);
--- src/bsu/rfe/java/group10/lab2/Nalivayko/var13/Main.java
+++ src/bsu/rfe/java/group10/lab2/Nalivayko/var13/Main.java
@@ -1,23 +1,8 @@
 package bsu.rfe.java.group10.lab2.Nalivayko.var13;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.ButtonGroup;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JTextField;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 
